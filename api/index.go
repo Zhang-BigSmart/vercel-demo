e2ad17af835f35bd71710670e998d319656da3fe
zzh <zzh@imlaidian.com>
2023-04-17 11:31:37 +0800

    init

--- api/index.go
+++ api/index.go
@@ -1,10 +1,29 @@
 package handler
 
 import (
-	"fmt"
+	"json"
 	"net/http"
 )
 
+// List 会返回给交付层一个列表回应
+type Result struct {
+	Code    int         `json:"code"`    //请求状态代码
+	Message interface{} `json:"message"` //请求结果提示
+	Data    interface{} `json:"data"`    //请求结果
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello! Smart Zhang</h1>")
+
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+
+	data := "Hello! Smart Zhang!"
+
+	response, _ := json.Marshal(&Result{
+		Code:    200,
+		Message: "ok",
+		Data:    data,
+	})
+
+	_, _ = w.Write(response)
+
 }
